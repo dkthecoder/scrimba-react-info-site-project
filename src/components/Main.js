215d5d069f750ea9691f1b992182b10332005789
dkthecoder <dkthecoder@coder.com>
2022-09-24 19:10:43 +0100

    added new image, changed colours/font types

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+                <h1 className="main--title">Main component</h1>
+                <ul className="main--facts">
+                    <li>was first released in 2013</li>
+                    <li>was originally created by JW</li>
+                    <li>100k stars on GitHub</li>
+                    <li>facebook owns it (well, maintains it)</li>
+                    <li>powers bare tings</li>
+                </ul>
+        </main>
+
+    )
+}
